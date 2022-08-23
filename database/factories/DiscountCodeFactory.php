afa5357432b063059e896e8f8ca3663266ec0d41
samanxzrpor <xzrpor@gmail.com>
2022-08-23 14:21:44 +0430

    add Discount code factory

--- database/factories/DiscountCodeFactory.php
+++ database/factories/DiscountCodeFactory.php
@@ -2,13 +2,19 @@
 
 namespace Database\Factories;
 
+use Domain\DiscountCode\Models\DiscountCode;
+use Domain\Webinar\Models\Webinar;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Model>
  */
 class DiscountCodeFactory extends Factory
 {
+
+    protected $model = DiscountCode::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +23,14 @@ class DiscountCodeFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => fake()->title(),
+            'discount_code' => Str::random(8),
+            'is_active' => random_int(0 , 1),
+            'start_date' => now()->addDays(random_int(0 , 20)),
+            'expires_date' => now()->addDays(30),
+            'discount_code_count' => random_int(1 , 500),
+            'discount_type' => Arr::random(['percentage', 'amount']),
+            'webinar_id' => fn() => Webinar::factory(),
         ];
     }
 }
--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -2,13 +2,20 @@
 
 namespace Database\Factories;
 
+use Domain\DiscountCode\Models\DiscountCode;
+use Domain\Orders\Models\Order;
+use Domain\User\Models\User;
+use Domain\Webinar\Models\Webinar;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Model>
  */
 class OrderFactory extends Factory
 {
+
+    protected $model = Order::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +24,10 @@ class OrderFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'status' => Arr::random(['paid', 'doing', 'unsuccessful']),
+            'discount_code_id' => fn() => DiscountCode::factory(),
+            'user_id' => fn() => User::factory(),
+            'webinar_id' => fn() => Webinar::factory(),
         ];
     }
 }
